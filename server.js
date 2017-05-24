31edcd4737a08e136784952454c76474b681e490
Kyle Hiebel <hiebelky@oregonstate.edu>
2017-05-24 14:54:38 -0700

    Empty server code

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var server = express();
 
-server.get("/", express.static("./public/"));
+server.get("/", function(req, res, next){
+	
+});
 
 server.listen(process.env.PORT || 3000, function(){
-	console.log("Listening on port", process.env.PORT || 3000)});
+	console.log("Listening on port", process.env.PORT || 3000)
+});
