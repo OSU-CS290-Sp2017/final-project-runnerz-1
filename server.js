171bdcf2e63c85c58579ae198d661ff8911f86b8
Kyle Hiebel <hiebelky@oregonstate.edu>
2017-06-03 12:00:58 -0700

    Modified code to work with express handlebars

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
 var fs = require("fs");
-var server = express();
 
-var indexHTML = fs.readFileSync("./public/index.html", "utf8");
+var server = express();
+server.engine("handlebars", exphbs({defaultLayout: "main"}));
+server.set("view engine", "handlebars");
+
+/*var indexHTML = fs.readFileSync("./public/index.html", "utf8");
 var calendarHTML = fs.readFileSync("./public/calendar.html", "utf8");
 var picturesHTML = fs.readFileSync("./public/pictures.html", "utf8");
 var officersHTML = fs.readFileSync("./public/officers.html", "utf8");
 var indexJS = fs.readFileSync("./public/index.js", "utf8");
 var styleCSS = fs.readFileSync("./public/style.css", "utf8");
-var HTML404 = fs.readFileSync("./public/404.html", "utf8");
+var HTML404 = fs.readFileSync("./public/404.html", "utf8");*/
 
-server.get("/*", function(req, res, next){
+server.get("/", function(req, res, next){
 	if(req.url == "/")
 	{
-		res.status(200);
-		res.end(indexHTML);
+		var args = {"title": "OSU Running Club"};
+		res.render("home", args);
 	}
 	if(req.url == "/calendar")
 	{
-		res.status(200);
-		res.end(calendarHTML);
+		var args = {"title": "OSU Running Club"};
+		res.render("calendar", args);
 	}
 	if(req.url == "/pictures")
 	{
-		res.status(200);
-		res.end(picturesHTML);
+		var args = {"title": "OSU Running Club"};
+		res.render("pictures", args);
 	}
 	if(req.url == "/officers")
 	{
-		res.status(200);
-		res.end(officersHTML);
+		var args = {"title": "OSU Running Club"};
+		res.render("officers", args);
 	}
 	if(req.url == "/style.css")
 	{
@@ -45,8 +49,8 @@ server.get("/*", function(req, res, next){
 });
 
 server.get("*", function(req, res, next){
-	res.status(404);
-	res.end(HTML404);
+	var args = {"title": "OSU Running Club (error 404)"};
+	res.render("404page", args);
 });
 
 server.listen(process.env.PORT || 3000, function(){
